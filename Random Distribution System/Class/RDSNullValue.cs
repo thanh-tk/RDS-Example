49768571462e25363c40802e4aee15f728b71ecf
Thanh Thái <haha_1st@outlook.com>
2022-07-05 16:49:47 +0700

    Commit first

---
+++ Random Distribution System/Class/RDSNullValue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Distribution_System.Class
+{
+    public class RDSNullValue : IRDSValue<object>
+    {
+        public RDSNullValue(double probablity) : base(null, probablity, false, false, true) { }
+    }
+}
---
+++ Random Distribution System/Interface/IRDS.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Distribution_System
+{
+    public interface IRDSObj
+    {
+        double rdsProbability {  get; set; } // The chance for this item to drop
+        bool rdsUnique { get; set; } // Only drops once per query
+        bool rdsAlways { get; set; } // Drops always
+        bool rdsEnabled { get; set; } // Can it drop now?
+
+        event EventHandler rdsPreResultEvaluation;
+        event EventHandler rdsHit;
+        event EventHandler rdsPosReultEvaluation;
+
+        void onRDSPreResultEvaluation(EventArgs e);
+
+        void onRDSHit(EventArgs e);
+        void onRDSPosResultEvaluation(EventArgs e);
+    }
+    public interface IRDSValue<T> : IRDSObj
+    {
+        T rdsValue { get; }
+    }
+    public interface IRDSTable : IRDSObj
+    {
+        int rdsCount { get; set; }
+        IEnumerable<IRDSObj> rdsContents { get; }
+        IEnumerable<IRDSObj> rdsResult { get; }
+    }
+
+}
